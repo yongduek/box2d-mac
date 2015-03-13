1819fef321f789319787a4c64f13f8de4ccd4049
Yongduek Seo <yndk@sogang.ac.kr>
2015-03-13 15:24:25 +0900

    initial

---
+++ v2.3.1/Box2D/bld/HelloWorld/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/HelloWorld/HelloWorld.cpp" "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/HelloWorld/CMakeFiles/HelloWorld.dir/HelloWorld.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ v2.3.1/Box2D/bld/Testbed/CMakeFiles/Testbed.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/Testbed/Framework/Main.cpp" "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/Testbed/CMakeFiles/Testbed.dir/Framework/Main.cpp.o"
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/Testbed/Framework/RenderGL3.cpp" "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/Testbed/CMakeFiles/Testbed.dir/Framework/RenderGL3.cpp.o"
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/Testbed/Framework/Test.cpp" "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/Testbed/CMakeFiles/Testbed.dir/Framework/Test.cpp.o"
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/Testbed/Tests/TestEntries.cpp" "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/Testbed/CMakeFiles/Testbed.dir/Tests/TestEntries.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "FREEGLUT_EXPORTS"
+  "FREEGLUT_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yndk/Dropbox/opengl/b2/v2.3.1/Box2D/bld/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks/OpenGL.framework"
+  ".."
+  "/usr/X11/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
